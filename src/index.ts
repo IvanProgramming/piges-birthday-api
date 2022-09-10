db824910fd55c7e2022109cae738bc19b7c863e3
Ivan <23701564+IvanProgramming@users.noreply.github.com>
2022-09-10 16:42:08 +0300

    Added /today/birthdays

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,62 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import {Hono} from 'hono'
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+const app = new Hono()
+
+class FullDate {
+    localizedString: string = ""
+    currentYear: string = ""
+    monthAndDate: string = ""
+
+    constructor(date: Date) {
+        this.localizedString = date.toLocaleString('ru-RU', {day: 'numeric', month: 'long'})
+        this.currentYear = date.toLocaleDateString('ru-RU')
+        this.monthAndDate = date.toLocaleDateString('ru-RU', {day: '2-digit', month: '2-digit', year: undefined})
+    }
 }
 
-export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
-};
+class Birthday {
+    date: FullDate
+    name: string
+
+    constructor(date: FullDate, name: string) {
+        this.date = date
+        this.name = name
+    }
+}
+
+
+const getData = async (fetcher: Fetcher, apiUrl: string, sheetsDbCredentials: string, kvNamespace: KVNamespace): Promise<Object[]> => {
+    let kvValue = await kvNamespace.get('cache')
+    if (kvValue) {
+        console.log("Used cached data")
+        return JSON.parse(<string>kvValue) as Object[]
+    }
+    let response = await fetcher.fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + btoa(sheetsDbCredentials)
+        }
+    })
+    let data = await response.json()
+    await kvNamespace.put('cache', JSON.stringify(data), {expirationTtl: 60})
+    return data as Object[]
+
+}
+
+app.get('/today/birthdays', async (c) => {
+    let data = await getData(c.req.fetcher!, c.env.SHEETS_DB_API_URL, c.env.SHEETS_DB_CREDENTIALS, c.env.KV_NAMESPACE)
+    let today = new Date()
+    let birthdays = []
+    for (let i = 0; i < data.length; i++) {
+        // @ts-ignore
+        let birthday = data[i]["День рождение"].trim()
+        if (birthday == today.toLocaleString("ru-RU", {day: 'numeric', month: 'long'})) {
+            // @ts-ignore
+            birthdays.push(new Birthday(new FullDate(today), data[i]["Минипиг"]))
+        }
+    }
+    return c.json({data: birthdays})
+})
+
+export default app
